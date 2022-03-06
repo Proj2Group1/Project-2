3d06e9cf38c06c7a1853b07dc5556f148f7af9b5
Christopher <programmerchristopher@gmail.com>
2022-03-06 00:10:40 -0800

    mealServing calculations are messed up for some reason but conference works

--- conference.c
+++ conference.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <math.h>
-//#include”functions.h”
+//#include ”functions.h”
 
-double validateMoney() {
-    double cost;
-    scanf("%lf", &cost);
-    cost = floorf(cost * 100) / 100;
-    return cost;
+double insertDoublePos() {
+    double cost = -1.0;
+    char input[1000];
+    char* cp, badc;
+    int n;
+    while (cost < 0)
+    {
+        cp = fgets(input, sizeof(input), stdin);
+        if (cp == input)
+        {
+            n = sscanf(input, "%lf %c", &cost, &badc);
+            if (n != 1)
+            {
+                printf("ERROR! Please enter a number: ");
+                cost = -1;
+            }
+            else if (cost < 0)
+                printf("ERROR! Please enter a positive number: ");
+            else
+                return cost;
+        }
+    }
 }
 
 double conferenceFee() {
-    double fee = validateMoney();
+    double fee = insertDoublePos();
     return fee;
 }
 
-int main() {
+/*int main() {
     printf("Please enter the conference fee: ");
     double fee = conferenceFee();
     printf("The fee is: $");
     printf("%.2f", fee);
     printf("\n");
     return 0;
-}
+}*/
--- mealServing.c
+++ mealServing.c
@@ -1,19 +1,67 @@
 #include <stdio.h>
-#include <math.h>
 //#include”functions.h”
 
-int countBreakfast(int days, int departureTime, int arrivalTime) {
+int countBreakfast(int* departureTime, int* arrivalTime) {
+    int count = 0;
 
+    if (departureTime[0] <= 6)
+        count++;
+    if (arrivalTime[0] > 8 && arrivalTime[0] <= 13)
+        count++;
+
+    printf("breakfasts: "); //DEBUGGING
+    printf("%d", count); //DEBUGGING
+    printf("   "); //DEBUGGING
+    return count;
 }
 
-int countLunch(int days, int departureTime, int arrivalTime) {
+int countLunch(int* departureTime, int* arrivalTime) {
+    int count = 0;
 
+    if (departureTime[0] >= 7 && departureTime[0] <= 11)
+        count++;
+    if (arrivalTime[0] > 13 && arrivalTime[0] <= 19)
+        count++;
+
+    printf("lunches: "); //DEBUGGING
+    printf("%d", count); //DEBUGGING
+    printf("   "); //DEBUGGING
+    return count;
 }
 
-int countDinner(int days, int departureTime, int arrivalTime) {
+int countDinner(int* departureTime, int* arrivalTime) {
+    int count = 0;
 
+    if (departureTime[0] >= 12 && departureTime[0] <= 17)
+        count++;
+    if (arrivalTime[0] > 19)
+        count++;
+
+    printf("dinners: "); //DEBUGGING
+    printf("%d", count); //DEBUGGING
+    printf("   "); //DEBUGGING
+    return count;
 }
 
-int companyCoveredFood(int breakfast, int lunch, int dinner) {
+double companyCoveredFood(int* departureTime, int* arrivalTime) {
+    double costCovered = 0.0;
 
+    costCovered += 9.00 * countBreakfast(departureTime, arrivalTime);
+    costCovered += 12.00 * countLunch(departureTime, arrivalTime);
+    costCovered += 16.00 * countDinner(departureTime, arrivalTime);
+
+    return costCovered;
+}
+
+//DEBUGGING below
+int main() {
+    int arr[] = {0, 0, 0};
+    for (int i = 0; i < 24; i++) {
+        printf("%d", i);
+        printf(":00     ");
+        printf("%.2f", companyCoveredFood(arr, arr));
+        printf("\n");
+        arr[0] = i;
+    }
+    return 0;
 }

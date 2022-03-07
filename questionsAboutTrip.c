44f13d7977636be95ca047c9c53a19e0dc4a0d74
Marian Remoroza <mrremoroza@cpp.edu>
2022-03-06 18:53:16 -0800

    Finished this function. Pls check and let me know if it's good.

--- questionsAboutTrip.c
+++ questionsAboutTrip.c
@@ -1,4 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdbool.h>
 //#include "functions.h"
+
+/* Asks the user for the total num of days on trip. 
+    returns the int number days */
+int getTotalDays(){
+    int totalDays;
+    printf("How many days was your trip?");
+    scanf("%d", &totalDays);
+    return totalDays;
+}
+
+/* Asks user for departure time.
+    returns time as int & in military time. */
+int *getDepartureTime(){
+    const int MAX = 2;
+    int counter;
+    int departureTime[2] = {0,0,0};
+    printf("Enter your departure time in 24-hour (military) format:\n"
+           "Hit enter to separate the hour and minutes.\n"
+           "Example input:\n"
+           "20\n00"); 
+    for(counter = 0; counter<MAX; counter++){
+        scanf("%d", &departureTime[counter]);
+    }
+    /* if want to print out the input: 
+    printf("&d:&d", (*ptr)[0],(*ptr)[1]); */
+    return departureTime;
+}
+
+/* Asks user for arrival time.
+    returns time as int & in military time. */
+int *getArrivalTime(){
+    // military time
+    const int MAX = 2;
+    int counter;
+    int arrivalTime;
+    int arrivalTime[2] = {0,0,0};
+    printf("Enter your arrival time in 24-hour (military) format:\n"
+           "Hit enter to separate the hour and minutes.\n"
+           "Example input:\n"
+           "7\n00");
+    for(counter = 0; counter<MAX; counter++){
+        scanf("%d", &departureTime[counter]);
+    }
+
+    return arrivalTime;
+}
+
+/* Asks the user if they rented a car.
+    returns true or false */
+bool rentedCar(){
+    bool renting;
+    int temp;
+    printf("Did you rent a car during your trip? 1 is true and 0 is false: ");
+    scanf("%d", &temp);
+    rented = temp;
+
+    if(renting){
+        return true;
+    }
+    else return false;
+}
+
+/* Passes one arg if car was rented or !rented.
+    if user rented, prompt how much the rental price was. 
+    else return false.
+    this will return the rent price as a double.  */
+double getPriceCarRentals(bool rented){
+    double price;
+    if (rented){
+        printf("Please enter your rental price per day: ");
+        scanf("%lf", &price);
+    }
+    else return 0;
+}
+
+/* Asks the user if they used a private car instead. 
+    returns true or false. */
+bool privateCar(){
+    bool private;
+    int temp;
+    printf("Did you use a private car during your trip? 1 is true and 0 is false: ");
+    scanf("%d", &temp);
+    private = temp;
+
+    if(private){
+        return true;
+    }
+    else return false;
+}

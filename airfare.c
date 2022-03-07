88424119a1db1e5e1aa89e2b3860b2162e132b61
Breanne Ha <98631762+bre-ha20@users.noreply.github.com>
2022-03-06 22:38:08 -0800

    fixing bugs

--- airfare.c
+++ airfare.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include "functions.h"
 
 //cost of entire airfare
 double airfareCost() {
 
     double total_airfareCost;
-    do {
+   
         printf("Enter the amount of any round-trip airfare: \n");
         scanf("%lf" , &total_airfareCost);
-    }
 
-    while (total_airfareCost < 0);{
+    while (total_airfareCost < 0) {
         printf("Error: Cannot enter a negative amount. \n");
     }
     
